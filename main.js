531cbfe3740bfa1d7c578cf3b0d1fe2af91aec90
Roger <luojie.5408@163.com>
2018-12-09 23:54:09 +0800

    update

--- main.js
+++ main.js
@@ -20,5 +20,5 @@ app.post('/upload',multipartMiddleware,upload);
 app.use('/', express.static('static'))
 
 const server = require('http').createServer(app)
-const port = 80
+const port = 9999
 server.listen(port, () => console.log(`Express server listening on :${port}`))
